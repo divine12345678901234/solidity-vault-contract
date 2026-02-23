8824cd85611ea15ca26cd21c3f371fb520d877bc
ezechiyere chinomso divine <ezechiyerechinomso123@gmail.com>
2026-02-24 00:27:07 +0100

    added a new function

--- contacts.sol
+++ contacts.sol
@@ -1,26 +1,46 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract TokenValidator{
-    mapping(address=>bool) private AddressToBool;
-    mapping (address=>string) private AddressToString;
-   string message1 = "input entered";
-    struct Addresses{
-        address MyWalletaddress;
+
+import "./new.sol";
+
+contract WalletValidator{
         
+    
+    mapping(address=>bool) private AddressToBool;
+    mapping(address=>string) private AddressToString;
+    string message1 = "you have entered an address";
+    struct Addresses{
+        address TokenAddress;
     }
-    Addresses[] private  addresses;
-    
-    
+
+    Addresses[] private addresses;
+
     
 
-    function isWhitelisted(address walletAddress) public {
-        require(!AddressToBool[walletAddress], "this address already exists");
-       
-        AddressToBool[walletAddress] = true;
-        addresses.push(Addresses(walletAddress));
-       
+    function whitelistToken(address TokenAddress) public {
+        
+        require(!AddressToBool[TokenAddress], "this address already exists");
+        AddressToBool[TokenAddress] = true;
+        addresses.push(Addresses(TokenAddress));
+        AddressToString[TokenAddress] = "ACTIVE";
+        
+        
+
+    }
+    function isWhitelisted(address TokenAddress) public view returns (bool){
+        return AddressToBool[TokenAddress];
+
+
+    
+        
+        
         
     }
+    function RemoveWhiteListedToken(address TokenAddress)public{
+        AddressToBool[TokenAddress] = false;
+    }
         
+   
+   
 }
